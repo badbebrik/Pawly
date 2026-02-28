a0e78c13b9cbf03c94bc94c64c8ee4b54715dae6
Viktoria Serikova <viktoria.serikova2016@yandex.ru>
2026-02-28 04:11:39 +0300

    fix environment problem while running on ios device

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,7 +1,10 @@
 class ApiConstants {
   const ApiConstants._();
 
-  static const String baseUrl = "http://localhost:8000";
+  static const String baseUrl = String.fromEnvironment(
+    'API_BASE_URL',
+    defaultValue: 'http://localhost:8000',
+  );
   static const Duration connectTimeout = Duration(seconds: 15);
   static const Duration receiveTimeout = Duration(seconds: 20);
   static const Duration sendTimeout = Duration(seconds: 20);
